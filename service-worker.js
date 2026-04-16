dd4b80c130b941c7f48e42796bb8cf8ff95391a4
abdouroihamaneelamine9-lang <abdouroihamaneelamine9@gmail.com>
2026-04-16 13:01:12 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,23 @@
-const CACHE_NAME = "arkpanter-v1";
+const CACHE_NAME = "routine-cache-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
 ];
 
-// 1. Installation → on stocke les fichiers
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 2. Activation → nettoyage ancien cache
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// 3. Hors ligne intelligent
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      // Si en cache → utiliser
-      if (response) return response;
-
-      // Sinon → internet + sauvegarde
-      return fetch(event.request).then(networkResponse => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
